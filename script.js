7258da5b1bdb449e866a5564ab1af62678219493
PorteManteau <nuno.enes.ltz@gmail.com>
2025-01-20 10:20:57 +0100

    update

--- script.js
+++ script.js
@@ -37,48 +37,53 @@ document.addEventListener('DOMContentLoaded', function() {
                     labels: labels,
                     datasets: [{
                         label: catastrophes[i],
-                        data: data
+                        data: data,
+                        pointRadius : 0,
+                        backgroundColor: 'rgba(0, 0, 0, 0)',
                     }]
                 }
             });
 
-            function updateChart(disasterType, label) {
+            function updateChart(disasterType, label, borderColor) {
                 let labels = Object.keys(disasterData[disasterType]);
                 let data = Object.values(disasterData[disasterType]);
 
                 chart.data.labels = labels;
                 chart.data.datasets[0].data = data;
                 chart.data.datasets[0].label = label;
+                chart.data.datasets[0].borderColor = borderColor;
                 chart.update();
             }
 
+            
             document.getElementById('btnall').addEventListener('click', function() {
                 i = 0;
-                updateChart('All disasters', catastrophes[i]);
+                updateChart('All disasters', catastrophes[i], '#FF0000');
             });
 
             document.getElementById('btnino').addEventListener('click', function() {
                 i = 3
-                updateChart('Flood', catastrophes[i]);
+                updateChart('Flood', catastrophes[i], '#0000FF');
             });
 
             document.getElementById('btnfeu').addEventListener('click', function() {
                 i = 7;
-                updateChart('Wildfire', catastrophes[i]);
+                updateChart('Wildfire', catastrophes[i], '#ff5100');
                 
             });
 
             document.getElementById('btntre').addEventListener('click', function() {
                 i = 1;
-                updateChart('Earthquake', catastrophes[i]);
+                updateChart('Earthquake', catastrophes[i], '#402402');
             });
 
             document.getElementById('btnsec').addEventListener('click', function() {
                 i = 2;
-                updateChart('Drought', catastrophes[i]);
+                updateChart('Drought', catastrophes[i], '#d98723');
             });
 
-            updateChart('All disasters');
+            updateChart('All disasters', catastrophes[i], '#FF0000');
+
 
         });
     
